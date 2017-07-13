7a4e0cfaaaf1a28cddf9cfa347a1524924d6f132
tobnys <tobnystrom@gmail.com>
2017-07-13 20:14:33 +0200

    test

--- index.js
+++ index.js
@@ -2,10 +2,6 @@
     const YT_API = "https://www.googleapis.com/youtube/v3/search";
 
 
-    $(".js-submit").on("click", function(e){
-        
-    });
-
     $(".js-submit-form").submit(function(){
         alert("trest")
     });
